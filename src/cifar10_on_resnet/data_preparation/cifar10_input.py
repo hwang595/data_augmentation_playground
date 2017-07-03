90cf26fd5ffc0dfe97a8dfc2c4f725ee609dabbd
hwang595 <hongyiwang@cs.wisc.edu>
2017-07-02 21:53:21 -0500

    some modifications

--- src/cifar10_on_resnet/data_preparation/cifar10_input.py
+++ src/cifar10_on_resnet/data_preparation/cifar10_input.py
@@ -148,12 +148,30 @@ def random_crop_and_flip(batch_data, padding_size):
 
     return cropped_batch
 
+def random_crop(batch, crop_shape, padding=None):
+  oshape = np.shape(batch[0])
+  if padding:
+    oshape = (oshape[0] + 2*padding, oshape[1] + 2*padding)
+  new_batch = []
+  npad = ((padding, padding), (padding, padding), (0, 0))
+  for i in range(len(batch)):
+    new_batch.append(batch[i])
+    if padding:
+      new_batch[i] = np.lib.pad(batch[i], pad_width=npad,
+                          mode='constant', constant_values=0)
+    nh = random.randint(0, oshape[0] - crop_shape[0])
+    nw = random.randint(0, oshape[1] - crop_shape[1])
+    new_batch[i] = new_batch[i][nh:nh + crop_shape[0],
+                              nw:nw + crop_shape[1]]
+#    r = np.linalg.norm(np.subtract(batch[i], new_batch[i]))
+  return np.array(new_batch)
+
 def aug_data_set(ori_data, ori_labels, times_expand=1):
     aug_data_list = []
     new_data=ori_data
     new_label=ori_labels
     for time_aug in range(times_expand):
-        crop_data = random_crop(ori_data, crop_shape=(32, 32), padding=2)   
+        crop_data = random_crop(ori_data, crop_shape=(32, 32), padding=1)   
         aug_data_list.append(crop_data)
         new_data = np.concatenate((new_data,aug_data_list[time_aug]),axis=0)
     new_label = np.concatenate((new_label,ori_labels), axis=0)
@@ -180,7 +198,7 @@ def prepare_train_data(padding_size):
 #    pad_width = ((0, 0), (padding_size, padding_size), (padding_size, padding_size), (0, 0))
 #    data = np.pad(data, pad_width=pad_width, mode='constant', constant_values=0)
     sampled_train_images, sampled_train_labels = down_sample(data, label, down_sample_num=1024)
-    train_set_new, train_labels_new = aug_data_set(sample_data, sample_labels, times_expand=68)
+    train_set_new, train_labels_new = aug_data_set(sampled_train_images, sampled_train_labels, times_expand=68)
     return train_set_new, train_labels_new
 #    return sampled_train_images, sampled_train_labels
 #    return data, label
